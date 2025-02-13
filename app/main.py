9395e62aa5fcce8f90ca0beb084b134bad9074a4
paulinamarkiewicz641 <paulinamarkiewicz4506@gmail.com>
2025-02-13 12:28:51 +0100

    Taks updated

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
         "source": "flake8"
     }
-    pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -16,16 +15,10 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "path": file_path,
         "status": "failed" if formatted_errors else "passed"
     }
-    pass
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-        "errors": [format_linter_error(error) for error in errors],
-        "path": file_path,
-        "status": "failed" if errors else "passed"
-        }
+        format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
         ]
-    pass
