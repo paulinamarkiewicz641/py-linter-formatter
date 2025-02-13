a132c3d532df9620712c74d2d19776c08d578645
paulinamarkiewicz641 <paulinamarkiewicz4506@gmail.com>
2025-02-13 12:51:41 +0100

    Task 3

--- app/main.py
+++ app/main.py
@@ -9,13 +9,10 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    formatted_errors = [
-        format_linter_error(error) for error in errors
-    ]
     return {
-        "errors": formatted_errors,
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if formatted_errors else "passed"
+        "status": "failed" if errors else "passed"
     }
 
 
