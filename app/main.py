7eb0fb4955dfd015522f0553015d641ce266612a
paulinamarkiewicz641 <paulinamarkiewicz4506@gmail.com>
2025-02-17 22:20:45 +0100

    Task 4

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
@@ -21,4 +20,4 @@ def format_linter_report(linter_report: dict) -> list:
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
-
+    
